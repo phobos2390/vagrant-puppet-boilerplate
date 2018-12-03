8e8101da95114652aacf93292aec941f03c03e92
Larsen, James <JamesB444@msn.com>
2018-12-02 20:07:14 -0500

    Gets  user defined provisions

--- puppet/environments/default/manifests/site.pp
+++ puppet/environments/default/manifests/site.pp
@@ -1,17 +1,27 @@
+include 'people::user'
+
 if versioncmp($::puppetversion, '3.6.1') >= 0 {
   Package {
     allow_virtual => true,
   }
 }
 
-$packages=hiera('packages')
+$packages=lookup('packages')
 
 package { $packages:
   ensure => installed,
 }
 
-$repos=hiera('repos')
-$repodefaults=hiera('repo-defaults')
+$repos=lookup('repos')
+$repodefaults=lookup('repo-defaults')
 
 create_resources(vcsrepo,$repos,$repodefaults)
+
+exec { "Install-Dotfiles":
+  command => "/bin/sh ./install.sh",
+  user => "vagrant",
+  cwd => "/home/vagrant/phobos2390/dotfiles",
+  creates => "/home/vagrant/.dotfiles/",
+  require => Vcsrepo["/home/vagrant/phobos2390/dotfiles"],
+}
 
